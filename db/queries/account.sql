ea5f5b978a5fee7d3b457fd30a738659e2348d63
Ian-Jay <ianjaymamansag995@gmail.com>
2025-05-31 20:02:54 +0800

    Initialize SQLC

---
+++ db/queries/account.sql
@@ -0,0 +1,27 @@
+-- name: CreateAccount :one
+INSERT INTO accounts (
+  owner,
+  balance,
+  currency
+) VALUES (
+  $1, $2, $3
+)
+RETURNING *;
+
+-- name: GetAccount :one
+SELECT * FROM accounts
+WHERE id_account = $1 LIMIT 1;
+
+-- name: ListAccounts :many
+SELECT * FROM accounts
+ORDER BY id_account
+LIMIT $1 OFFSET $2;
+
+-- name: UpdateAccount :exec
+UPDATE accounts
+  set balance = $2
+WHERE id_account = $1;
+
+-- name: DeleteAccount :exec
+DELETE FROM accounts
+WHERE id_account = $1;
